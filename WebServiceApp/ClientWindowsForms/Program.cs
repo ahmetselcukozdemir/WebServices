f85cf8ce18e23312f405fd52301698471496fd3b
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2020-01-16 23:42:56 +0300

    commit3

--- WebServiceApp/ClientWindowsForms/Program.cs
+++ WebServiceApp/ClientWindowsForms/Program.cs
@@ -16,7 +16,7 @@ namespace ClientWindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TcknDogrula());
         }
     }
 }
---
+++ WebServiceApp/ClientWindowsForms/TcknDogrula.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClientWindowsForms.TcknServiceReference;
+namespace ClientWindowsForms
+{
+    public partial class TcknDogrula : Form
+    {
+        public TcknDogrula()
+        {
+            InitializeComponent();
+        }
+
+        private void TcknDogrula_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnDogrula_Click(object sender, EventArgs e)
+        {
+            long tckn = Convert.ToInt64(mskTckn.Text);
+            string isim = txtIsim.Text.ToUpper();
+            string soyisim = txtSoyisim.Text.ToUpper();
+            int dogumyili = Convert.ToInt32(txtDogumYili.Text);
+
+            KPSPublicSoap client=new KPSPublicSoapClient();
+            bool sonuc = client.TCKimlikNoDogrula(tckn, isim, soyisim,dogumyili);
+            if (sonuc)
+            {
+                MessageBox.Show("Tckn doğru");
+            }
+            else
+            {
+                MessageBox.Show("tckn yanlış");
+            }
+
+
+        }
+    }
+}
